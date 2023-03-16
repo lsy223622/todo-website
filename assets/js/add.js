f56d51bf28a49d11d022f1d15841804e762bffdf
lsy223622 <lsy223622@outlook.com>
2023-03-16 17:23:38 +0800

    格式化

---
+++ assets/js/add.js
@@ -0,0 +1,45 @@
+function addTask() {
+  const title = document.getElementById("title").value;
+  const content = document.getElementById("content").value;
+  const deadline = document.getElementById("deadline").value;
+  const priority = document.getElementById("priority").value;
+  const finished = document.getElementById("finished").checked;
+
+  const addTime = new Date().toISOString().slice(0, 19).replace("T", " ");
+
+  const task = {
+    title,
+    content,
+    addTime,
+    deadline,
+    priority,
+    finished,
+  };
+
+  const sessionKey = getCookie("sessionKey");
+  const userId = getCookie("userId");
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "add.php", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const response = xhr.responseText;
+      if (response === "OK") {
+        alert("Task added!");
+      }
+    }
+  };
+  xhr.send(JSON.stringify({ sessionKey, userId, task }));
+}
+
+function getCookie(name) {
+  const cookies = document.cookie.split("; ");
+  for (const cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.split("=");
+    if (cookieName === name) {
+      return cookieValue;
+    }
+  }
+  return "";
+}
